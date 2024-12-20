1eea60a29e17f54a4c6f5089c4b30019a15f2b94
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-12-20 21:25:14 +0530

    Permanent Storage Feature:  - Word.fromCsv, Word.toCsv, Word.equals are added to support repository.  - WordRepository updated with file system based storage.  - WordRepository.write created to write single Word to a file. Additional:  - ignore 'store.txt'

--- cui/src/main/dev/abhisek/cui/repository/WordRepository.java
+++ cui/src/main/dev/abhisek/cui/repository/WordRepository.java
@@ -2,39 +2,100 @@ package main.dev.abhisek.cui.repository;
 
 import main.dev.abhisek.cui.entities.Word;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WordRepository {
-    private final List<Word> words;
+    private static final String STORAGE_PATH = "./store.txt";
+    private  File file;
 
     public WordRepository() {
-        words = new ArrayList<>();
+        try {
+            file = new File(STORAGE_PATH);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            file = null;
+            throw new RuntimeException(e);
+        }
     }
 
     public void add(Word word) {
-        if(words.isEmpty()){
-            words.add(word);
+        List<Word> words = getAll();
+        if (words.isEmpty()) {
+            write(word);
             return;
         }
         boolean found = false;
-        for(Word w : words){
-            if(w.getName().equals(word.getName())){
+        for (Word w : words) {
+            if (w.getName().equals(word.getName())) {
                 found = true;
                 break;
             }
         }
-        if(!found){
-            words.add(word);
+        if (!found) {
+            write(word);
+        }
+    }
+
+    private void write(Word word) {
+        try {
+            FileOutputStream fos = new FileOutputStream(file, true);
+            String wordAsCsv = Word.toCsv(word);
+            fos.write(wordAsCsv.getBytes());
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
 
     public List<Word> getAll() {
-        return words;
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            byte[] bytes = fis.readAllBytes();
+            fis.close();
+            StringBuilder line = new StringBuilder();
+            List<String> lines = new ArrayList<>();
+            for (byte b : bytes) {
+                char c = (char) b;
+                if (c == '\n') {
+                    lines.add(line.toString());
+                    line = new StringBuilder();
+                } else {
+                    line.append(c);
+                }
+            }
+            List<Word> words = new ArrayList<>();
+            for (String s : lines) {
+                Word word = Word.fromCsv(s);
+                words.add(word);
+            }
+            return words;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return List.of();
+        }
     }
 
     public void remove(Word word) {
-        words.remove(word);
+        try {
+            List<Word> words = getAll();
+            words.remove(word);
+
+            StringBuilder text = new StringBuilder();
+            FileOutputStream fos = new FileOutputStream(file);
+            for (Word w : words) {
+                text.append(Word.toCsv(w));
+            }
+            fos.write(text.toString().getBytes());
+            fos.flush();
+            fos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
